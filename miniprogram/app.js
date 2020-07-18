78237157dfaa7a0cf587ace5f253b7110e8697f5
Dunabee <1042513970@qq.com>
2020-07-18 21:09:13 +0800

    登录逻辑优化

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,20 +1,39 @@
 //app.js
 App({
+  globalData: {
+    openid: null,
+    userInfo: null
+  },
+
   onLaunch: function () {
-    
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
       wx.cloud.init({
-        // env 参数说明：
-        //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
-        //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
-        //   如不填则使用默认环境（第一个创建的环境）
-        // env: 'my-env-id',
+        // env: 'treehole-926ja',
         traceUser: true,
       })
     }
-
-    this.globalData = {}
+    var that = this
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          console.log("已授权过信息")
+          wx.getUserInfo({
+            success: res => {
+              that.globalData.userInfo = res.userInfo
+              if (that.userInfoReadyCallback) {
+                that.userInfoReadyCallback(res)
+              }
+            }
+          })
+        } else {
+          wx.showToast({
+            title: '请前往我的进行授权登录',
+            icon: 'none'
+          })
+        }
+      }
+    })
   }
 })
