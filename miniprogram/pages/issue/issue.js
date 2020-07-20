0562bf1d1ef34ad6b829de3debd9464d5e71c0de
Dunabee <1042513970@qq.com>
2020-07-20 22:59:53 +0800

    issue
    完善发布页面功能

--- miniprogram/pages/issue/issue.js
+++ miniprogram/pages/issue/issue.js
@@ -1,11 +1,148 @@
 // miniprogram/pages/issue/issue.js
+var app = getApp();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    uploaderImgList: [],
+    uploaderImgNum: 0,
+    showUploader: true,
+    cloudImgUrl: [],
+    issue: ''
+  },
 
+  clearImg(e) {
+    var nowList = [];
+    var uploaderImgList = this.data.uploaderImgList;
+    for (let i = 0; i < uploaderImgList.length; i++) {
+      if (i == e.currentTarget.dataset.index) {
+        continue
+      } else {
+        nowList.push(uploaderImgList[i])
+      }
+    }
+    this.setData({
+      uploaderImgList: nowList,
+      uploaderImgNum: this.data.uploaderImgNum - 1,
+      showUploader: true
+    })
+    console.log(this.data.uploaderImgList)
+  },
+  showImg(e) {
+    var that = this;
+    wx.previewImage({
+      urls: that.data.uploaderImgList,
+      current: that.data.uploaderImgList[e.currentTarget.dataset.index]
+    })
+  },
+  upload(e) {
+    var that = this;
+    wx.chooseImage({
+      count: 9 - that.data.uploaderImgNum,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success(res) {
+        console.log(res)
+        let tempFilePath = res.tempFilePaths;
+        let uploaderImgList = that.data.uploaderImgList.concat(tempFilePath);
+        if (uploaderImgList.length == 9) {
+          that.setData({
+            showUploader: false
+          })
+        }
+        that.setData({
+          uploaderImgList: uploaderImgList,
+          uploaderImgNum: uploaderImgList.length
+        })
+        wx.showLoading({
+          title: '上传中',
+        })
+        const cloudImgUrl = []
+        tempFilePath.forEach((item, i) => {
+          cloudImgUrl.push((new Date()).getTime() + Math.floor(10 * Math.random()) + tempFilePath[i].match(/\.[^.]+?$/)[0])
+        })
+        console.log('cloudImgUrl:' + cloudImgUrl)
+        tempFilePath.forEach((item, i) => {
+          wx.cloud.uploadFile({
+            cloudPath: cloudImgUrl[i],
+            filePath: tempFilePath[i],
+            success: res => {
+              console.log(res.fileID)
+              let cloudImgUrl= that.data.cloudImgUrl.concat(res.fileID)
+              that.setData({
+                cloudImgUrl:cloudImgUrl
+              })
+              wx.hideLoading()
+            },
+            fail: res => {
+              wx.hideLoading()
+              console.log(res)
+            }
+          })
+        })
+      }
+    })
+  },
+
+  uploadArticle() {
+    if (this.data.issue == '') {
+      wx.showToast({
+        title: '请输入内容',
+        icon: 'none'
+      })
+      return;
+    }
+    const treeholeDB = wx.cloud.database('treehole')
+    const articleList = treeholeDB.collection('articleList')
+    articleList.add({
+      data: {
+        author: app.globalData.userInfo.nickName,
+        issue: this.data.issue,
+        pic_url: this.data.cloudImgUrl,
+        postdate: this.formatTime(new Date())
+      },
+      success: res => {
+        wx.showToast({
+          title: '发布成功！',
+        })
+        this.setData({
+          uploaderImgList: [],
+          uploaderImgNum: 0,
+          showUploader: true,
+          cloudImgUrl: '',
+          issue: ''
+        })
+      },
+      fail: err => {
+        wx.showToast({
+          title: '发布失败！',
+        })
+      }
+    })
+  },
+
+  bindInput(e) {
+    this.setData({
+      issue: e.detail.value
+    })
+  },
+
+  formatTime:function(date){
+    var year=date.getFullYear()
+    var month=date.getMonth()+1
+    var day=date.getDate()
+    var hour=date.getHours()
+    var minute=date.getMinutes()
+    var second=date.getSeconds()
+    return[year,month,day].map(this.formatNumber).join('/')+' '+[hour,minute,second].map(this.formatNumber).join(':')
+  },
+
+  formatNumber:function(n){
+    n=n.toString()
+    return n[1]?n:'0'+n
   },
 
   /**
