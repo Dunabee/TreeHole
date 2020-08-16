a55a5965750dbc2f9839ba482538e0589b565291
Dunabee <1042513970@qq.com>
2020-08-16 12:46:33 +0800

    article

--- miniprogram/components/articleList/articleList.js
+++ miniprogram/components/articleList/articleList.js
@@ -1,12 +1,13 @@
 // components/picture-card/picture-card.js
+const app = getApp()
 Component({
   /**
    * 组件的属性列表
    */
   properties: {
-    articleList:{
-      type:Array,
-      value:[]
+    articleList: {
+      type: Array,
+      value: []
     }
   },
 
@@ -14,13 +15,51 @@ Component({
    * 组件的初始数据
    */
   data: {
-    
+    imgSize: {}
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    /*imageLoad(e) {
+      var originWidth = e.detail.width,
+        originHeight = e.detail.height,
+        ratio = originWidth / originHeight,
+        index = e.currentTarget.dataset.i,
+        image=this.data.imgSize,
+        that=this,
+        viewWidth=null,
+        viewHeight=null;
+      const query = wx.createSelectorQuery();
+      query.select('.image-section').boundingClientRect(function (res) {
+        viewWidth=res.width;
+        viewHeight=res.height;
+        var viewRatio=viewWidth/viewHeight;
+        if (ratio >= viewRatio) {
+          if (originWidth >= viewWidth) {
+            viewHeight = viewWidth / ratio;
+          } else {
+            viewWidth = originWidth;
+            viewHeight = originHeight
+          }
+        } else {
+          if (originWidth >= viewWidth) {
+            viewWidth = viewRatio * originHeight
+          } else {
+            viewWidth = viewRatio * originWidth;
+            viewHeight = viewRatio * originHeight;
+          }
+        }
+        image[index]={
+          width:viewWidth,
+          height:viewHeight
+        }
+        that.setData({
+          imgSize:image
+        })
+      })
+      query.exec();
+    }*/
   }
 })
